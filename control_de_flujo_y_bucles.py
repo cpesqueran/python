21ab1792cc418bc5059b92b9f6411ad47929df6e
Carlos Pesquera <cpesqueran@gmail.com>
2023-05-12 22:57:10 +0200

    Actualizacion tipos de datos y nuevo fich control

---
+++ control_de_flujo_y_bucles.py
@@ -0,0 +1,10 @@
+tipo = 'coche'
+
+if tipo == 'coche':
+    ruedas = 4
+elif tipo == 'bicicleta':
+    ruedas = 2
+elif tipo == 'camion':
+    ruedas = 6
+else:
+    ruedas = -1
--- tipos_de_datos.py
+++ tipos_de_datos.py
@@ -1,7 +1,8 @@
 # tipos_de_datos.py
 
 # Numeros
-x, y = 1, 4
+print('----------Numeros----------')
+x, y = 1, 4 # Asignaciones múltiples
 z = x
 
 print(x, y, z)
@@ -11,6 +12,7 @@ z = 7
 print(x, y, z)
 
 # Operaciones
+print('\n----------Operaciones----------')
 entero = 1
 decimal = 9.5
 
@@ -25,13 +27,33 @@ print(suma)
 print(decimal)
 
 # Cadenas
-cadena = 'cadena de texto'
-cadena2 = cadena
+print('\n----------Cadenas----------')
+cadena = 'Cadena de texto con comillas simples.'
+cadena2 = "Cadena de texto con comillas dobles y salto.\n"
+cadena3 = "Esto es una frase 'corta' usando comillas con salto.\n"
+cadena4 = 'Esto es una frase "corta" usando comillas con salto.\n'
+cadena5 = '''Esto es una frase usando comillas 'simples' y "dobles" en la \
+frase.\n'''
+
+parrafo1 = 'Frase simple\n \
+    convertida en parrafo.\n'
+parrafo2 = '''Esto es un parrafo sin poner ningun tipo de salto de linea entre
+cada una de las frases
+que la componen.\n''' 
 
 print(cadena)
 print(cadena2)
+print(cadena3)
+print(cadena4)
+print(cadena5)
+print(parrafo1)
+print(parrafo2)
+
+i = 6
+print(f'Esto es el valor de {i}\n')
 
 # Listas
+print('----------Listas----------')
 lst = [12, 3, 9]
 lst.append(10.1)
 
@@ -40,16 +62,19 @@ lst_copia[2] = 20
 
 print(lst, lst_copia)
 print(lst_copia.index(20))
+print(len(lst_copia))
 
 lst_copia.insert(2, 'Carlos')
 del lst_copia[0]
 print(lst_copia)
 
 # Tuplas
+print('\n----------Tuplas----------')
 tupla = (2, 3, 4, 'car')
-print(tupla)
+print(f'{tupla}\n')
 
 # Rangos
+print('----------Rangos----------')
 numero_inicio = -5
 numero_final = 10
 paso = 2
